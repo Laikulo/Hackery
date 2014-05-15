a32f0013c4c7cf2d5364ef0dc8d0d42e74c23a11
Aaron Haun <kj4ips@antichthon.net>
2014-05-14 22:15:17 -0400

    Auto Commit! Wed May 14 22:15:18 EDT 2014

--- autocommmit.sh
+++ autocommmit.sh
@@ -3,4 +3,5 @@ do
 	sleep  15m
 	git add .
 	git commit -m "Auto Commit! `date`"
+	git push
 done
--- src/main/java/guru/haun/hackery/worldgen/GlitchPlacer.java
+++ src/main/java/guru/haun/hackery/worldgen/GlitchPlacer.java
@@ -1,5 +1,7 @@
 package guru.haun.hackery.worldgen;
 
+import guru.haun.hackery.HackeryMod;
+
 import java.util.Random;
 
 import net.minecraft.world.World;
@@ -7,6 +9,8 @@ import net.minecraft.world.chunk.IChunkProvider;
 import cpw.mods.fml.common.IWorldGenerator;
 
 public class GlitchPlacer implements IWorldGenerator {
+	
+	private HackeryMod instance;
 	
 	public void generate(Random rng, int cX, int cZ, World world, IChunkProvider chunkgen, IChunkProvider chunkprov) {
 		//Overworld ONLY!!
@@ -16,7 +20,24 @@ public class GlitchPlacer implements IWorldGenerator {
 		
 		int originX = cX << 4 + rng.nextInt(16);
 		int originZ = cZ << 4 + rng.nextInt(16);
-		int originY = rng.NextInt(47) + 3;
+		int originY = rng.nextInt(47) + 3;
+		
+		int lengthX = rng.nextInt(3) + 3;
+		int lengthZ = rng.nextInt(3) + 3;
+		int lengthY = rng.nextInt(3) + 6;
+		
+		int centerX = originX + (lengthX >> 1);
+		int centerZ = originZ + (lengthZ >> 1);
+		int centerY = originY + (lengthY >> 1);
+		
+		for(int x = originX; x < originX+lengthX; x++){
+			for(int y = originY; y < originY+lengthY;y++){
+				for(int z = originZ; y < originZ+lengthZ;z++){
+						
+				}
+			}
+		}
+		
 	}
 
 }
