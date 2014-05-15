d29d0ada6979fabf58dfee1fb85c16f5af0b052c
Aaron Haun <kj4ips@antichthon.net>
2014-05-14 20:26:40 -0400

    Skekleton mod

---
+++ src/main/java/guru/haun/hackery/HackeryMod.java
@@ -0,0 +1,32 @@
+package guru.haun.hackery;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+
+@Mod(modid="hackery", name="Hackers", version = "modjam")
+public class HackeryMod {
+	
+
+	@EventHandler
+	public void HackeryPreInit(FMLPreInitializationEvent e){
+		
+	}
+	
+	
+	@EventHandler
+	public void HackeryInit(FMLInitializationEvent e){
+		
+	}
+	
+	
+	@EventHandler
+	public void HackeryPostInit(FMLPostInitializationEvent e){
+		
+	}
+	
+	
+}
