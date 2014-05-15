702dcbaa882b1cb7f04e7136bbde96c824735bad
Aaron Haun <kj4ips@antichthon.net>
2014-05-14 21:57:29 -0400

    Auto Commit! Wed May 14 21:57:29 EDT 2014

--- src/main/java/guru/haun/hackery/worldgen/GlitchPlacer.java
+++ src/main/java/guru/haun/hackery/worldgen/GlitchPlacer.java
@@ -1,5 +1,18 @@
 package guru.haun.hackery.worldgen;
 
-public class GlitchPlacer {
+import java.util.Random;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import cpw.mods.fml.common.IWorldGenerator;
+
+public class GlitchPlacer implements IWorldGenerator {
+	
+	public void generate(Random rng, int cX, int cZ, World world, IChunkProvider chunkgen, IChunkProvider chunkprov) {
+		//Overworld ONLY!!
+		if(!world.provider.isSurfaceWorld() ) return;
+		//Rarity Set
+		if(rng.nextInt(100) != 0) return;
+	}
 
 }
