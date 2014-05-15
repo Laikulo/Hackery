82fc8e120c00dcbdbabdf3e781d2fbb5b7371b85
Aaron Haun <kj4ips@antichthon.net>
2014-05-15 12:55:30 -0400

    Auto Commit! Thu May 15 12:55:31 EDT 2014

--- src/main/java/guru/haun/hackery/blocks/GlitchBlock.java
+++ src/main/java/guru/haun/hackery/blocks/GlitchBlock.java
@@ -1,6 +1,5 @@
 package guru.haun.hackery.blocks;
 
-import sun.awt.windows.WWindowPeer;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
@@ -21,69 +20,9 @@ public class GlitchBlock extends Block {
 		setBlockName("blockGlitch");
 		setLightOpacity(3);
 		setBlockTextureName("hackery:Glitch");
-		
+		setBlockBounds(.35F,.35F,.35F,.65F,.65F,.65F);
 	}
 	
-	void setBoundsFromMetaData(IBlockAccess ba, int x, int y, int z) {
-		float x1 = .35F;
-		float y1 = .35F;
-		float z1 = .35F;
-		float x2 = .65F;
-		float y2 = .65F;
-		float z2 = .65F;
-		
-		switch(ba.getBlockMetadata(x, y, z)){
-		case 1:
-			x1 = 0F;
-			x2 = 1F;
-			break;
-		case 2:
-			y1 = 0F;
-			y2 = 1F;
-			break;
-		case 3:
-			y1 = 0F;
-			y2 = 1F;
-			break;
-		default:
-			break;
-		}
-		this.setBlockBounds(x1, y1, z1, x2, y2, z2);
-	}
-	
-	public int onBlockPlaced(World w, int x, int y, int z, int side, float i, float j, float k, int metadata ){
-		return 0;
-	}
-	
-	private int ditermineMetadata(World w, int x, int y, int z){
-		if(w.getBlock(x + 1, y, z).equals(this) || w.getBlock(x - 1, y, z).equals(this)){
-			return 1;
-		}
-		if(w.getBlock(x, y + 1, z).equals(this) || w.getBlock(x, y - 1, z).equals(this)){
-			return 2;
-		}
-		if(w.getBlock(x, y, z + 1).equals(this) || w.getBlock(x, y, z - 1).equals(this)){
-			return 3;
-		}
-		return 0;
-	}
-	
-	
-	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z){
-		this.setBoundsFromMetaData(world, x, y, z);
-		return super.getCollisionBoundingBoxFromPool(world, x, y, z);
-	}
-	
-	@SideOnly(Side.CLIENT)
-	public AxisAlignedBB getSelectedBoundingBoxFromPool(World world, int x, int y, int z){
-		this.setBoundsFromMetaData(world, x, y, z);
-		return super.getCollisionBoundingBoxFromPool(world, x, y, z);
-	}
-	
-	public MovingObjectPosition collisionRayTrace(World w, int x, int y, int z, Vec3 a, Vec3 b){
-		this.setBoundsFromMetaData(w, x, y, z);
-		return super.collisionRayTrace(w, x, y, z, a, b);
-	}
 	
 	@SideOnly(Side.CLIENT)
 	public boolean renderAsNormalBlock() {
---
+++ src/main/java/guru/haun/hackery/items/GlitchHarvester.java
@@ -0,0 +1,16 @@
+package guru.haun.hackery.items;
+
+import java.util.Set;
+
+import net.minecraft.item.ItemTool;
+
+public class GlitchHarvester extends ItemTool {
+
+	protected GlitchHarvester(float speedOnProper, ToolMaterial creationMaterial,
+			Set properBlocks) {
+		super(p_i45333_1_, p_i45333_2_, p_i45333_3_);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
