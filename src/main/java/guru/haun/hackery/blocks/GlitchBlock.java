dd144d1f8cc42b75e7af1532a92e7eda4dee97fb
Aaron Haun <kj4ips@antichthon.net>
2014-05-15 01:15:38 -0400

    Auto Commit! Thu May 15 01:15:39 EDT 2014

--- src/main/java/guru/haun/hackery/blocks/GlitchBlock.java
+++ src/main/java/guru/haun/hackery/blocks/GlitchBlock.java
@@ -1,9 +1,11 @@
 package guru.haun.hackery.blocks;
 
+import sun.awt.windows.WWindowPeer;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.world.World;
 
 public class GlitchBlock extends Block {
 
@@ -11,9 +13,26 @@ public class GlitchBlock extends Block {
 		super(mat);
 		setResistance(9000000);
 		setBlockName("blockGlitch");
-		setBlockBounds(.35F,.35F,.35F,.65F,.65F,.65F);
 		setLightOpacity(3);
 		setBlockTextureName("hackery:Glitch");
+		setBlockBounds(.35F,.35F,.35F,.65F,.65F,.65F);
+	}
+	
+	public void setBlockBoundsBasedOnState(World w, int x, int y, int z) {
+		if(w.getBlock(x+1, y, z).equals(this) || w.getBlock(x-1, y, z).equals(this)){
+			this.setBlockBounds(0F,.35F,.35F,1F,.65F,.65F);
+			return;
+		}
+		if(w.getBlock(x, y+1, z).equals(this) || w.getBlock(x, y-1, z).equals(this)){
+			this.setBlockBounds(.35F,0F,.35F,.65F,1F,.65F);
+			return;
+		}
+		if(w.getBlock(x, y, z+1).equals(this) || w.getBlock(x, y, z-1).equals(this)){
+			this.setBlockBounds(.35F,.35F,0F,.65F,.65F,1F);
+			return;
+		}
+		this.setBlockBounds(.35F,.35F,.35F,.65F,.65F,.65F);
+		
 	}
 	
 	@SideOnly(Side.CLIENT)
