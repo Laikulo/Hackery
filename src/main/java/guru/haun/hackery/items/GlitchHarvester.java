e1ed054a4fb1d5f78e71726ece29ef833100db27
Aaron Haun <kj4ips@antichthon.net>
2014-05-15 17:12:17 -0400

    Auto Commit! Thu May 15 17:12:17 EDT 2014

--- src/main/java/guru/haun/hackery/items/GlitchHarvester.java
+++ src/main/java/guru/haun/hackery/items/GlitchHarvester.java
@@ -8,12 +8,15 @@ import com.google.common.collect.Sets;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatStyle;
 import net.minecraft.world.World;
 
 public class GlitchHarvester extends ItemTool {
@@ -44,9 +47,17 @@ public class GlitchHarvester extends ItemTool {
 		setTextureName("hackery:GlitchHarvester");
 	}
 	
-	public float getDamageVsEntity(){
-		return 0F;
+	//Not a sword
+	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entitiy){
+		if(!player.worldObj.isRemote){
+			player.addChatMessage(new ChatComponentText("<").appendSibling(
+					new ChatComponentText("fs|r5g").setChatStyle(new ChatStyle().setObfuscated(true)).appendSibling(
+					new ChatComponentText("That is not a sword, it is a expensive piece of equipment!")))
+					);
+		}
+		return true; //stop the attack
 	}
+	
 	
 	public Set<String> getToolClasses(ItemStack stack){
 		return Sets.newHashSet(new String[] {"GlitchHarvester"});
