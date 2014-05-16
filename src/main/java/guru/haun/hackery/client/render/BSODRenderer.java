6a73b5eef4f10a4614aee29a3069de76391e2b2b
Aaron Haun <kj4ips@antichthon.net>
2014-05-16 17:50:29 -0400

    Auto Commit! Fri May 16 17:50:30 EDT 2014

--- src/main/java/guru/haun/hackery/client/render/BSODRenderer.java
+++ src/main/java/guru/haun/hackery/client/render/BSODRenderer.java
@@ -6,13 +6,14 @@ import guru.haun.hackery.HackeryMod;
 import guru.haun.hackery.client.HacKeyBinds;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import cpw.mods.fml.common.gameevent.InputEvent;
 
-public class BSODRenderer {
+public class BSODRenderer extends Gui {
 	private static boolean active = false;
 	
 	@SubscribeEvent
@@ -23,10 +24,16 @@ public class BSODRenderer {
 		if(active){
 			int x = e.resolution.getScaledWidth();
 			int y = e.resolution.getScaledHeight();
+			int xc = x >> 1;
+			int yc = y >> 1;
+			int h = 287;
+			int w = 234;
+			int xi = w >> 1;
+			int yi = h >> 1;
+			ResourceLocation res = new ResourceLocation("hackery:bsods/Sac_Mac.png");
 			GL11.glColor4f(1F, 1F, 1F, 1F);
-			Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("hackery:bsods/Sac_Mac.png"));
-			Tesselator tess = new Tessellator().instance;
-			
+			Minecraft.getMinecraft().renderEngine.bindTexture(res);
+			this.drawTexturedModalRect(xc-xi, yc-yi, w, h, 0, 0);
 		}
 	}
 	
