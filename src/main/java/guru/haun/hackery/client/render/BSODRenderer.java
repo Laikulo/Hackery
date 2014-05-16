7aed60346b76eeb3d4aa64415f04198ff658c5c0
Aaron Haun <kj4ips@antichthon.net>
2014-05-16 19:50:45 -0400

    Auto Commit! Fri May 16 19:50:45 EDT 2014

--- src/main/java/guru/haun/hackery/client/render/BSODRenderer.java
+++ src/main/java/guru/haun/hackery/client/render/BSODRenderer.java
@@ -4,15 +4,20 @@ import org.lwjgl.opengl.GL11;
 
 import guru.haun.hackery.HackeryMod;
 import guru.haun.hackery.client.HacKeyBinds;
+import cpw.mods.fml.client.GuiScrollingList;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiScreenLongRunningTask;
 import net.minecraft.client.gui.ScaledResolution;
-import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import cpw.mods.fml.common.gameevent.InputEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.gui.GuiWinGame;
 
 public class BSODRenderer extends Gui {
 	public static boolean active = false;
@@ -20,7 +25,16 @@ public class BSODRenderer extends Gui {
 	private boolean oldguistate;
 	
 	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
 	public void onRenderGameOverlayEvent(RenderGameOverlayEvent e){
+		if(laststate == active){
+			if(active){
+				Minecraft.getMinecraft().currentScreen = GuiWinGame;
+			}else{
+				
+			}
+			laststate = active;
+		}
 		if(active){
 			int x = e.resolution.getScaledWidth();
 			int y = e.resolution.getScaledHeight();
@@ -30,26 +44,11 @@ public class BSODRenderer extends Gui {
 			int h = 256;
 			int xi = w >> 1;
 			int yi = h >> 1;
-			
-			Tessellator tess = Tessellator.instance;
-			GL11.glEnable(GL11.GL_BLEND);
-			GL11.glDisable(GL11.GL_TEXTURE_2D);
-			OpenGlHelper.glBlendFunc(770, 771, 1, 0);
-			GL11.glColor4f(0, 0, 0, 1F);
-			
-			tess.startDrawingQuads();
-			tess.addVertex((double) 0, (double) y, 0D);
-			tess.addVertex((double) x, (double) y, 0D);
-			tess.addVertex((double) x, (double) 0, 0D);
-			tess.addVertex((double) 0, (double) 0, 0D);
-			tess.draw();
-			
-			GL11.glEnable(GL11.GL_TEXTURE_2D);
-			GL11.glDisable(GL11.GL_BLEND);
-			
-			//ResourceLocation res = new ResourceLocation("hackery:bsods/Sad Notch.png");
-			//Minecraft.getMinecraft().renderEngine.bindTexture(res);
-			//this.drawTexturedModalRect(xc-xi, yc-yi, 0, 0, w, h);
+			ResourceLocation res = new ResourceLocation("hackery:bsods/Sad Notch.png");
+			this.drawRect(0, 0, x, y, 0xFF000000);
+			GL11.glColor4f(1F, 1F, 1F, 1F);
+			Minecraft.getMinecraft().renderEngine.bindTexture(res);
+			this.drawTexturedModalRect(xc-xi, yc-yi, 0, 0, w, h);
 		}
 	}
 	
