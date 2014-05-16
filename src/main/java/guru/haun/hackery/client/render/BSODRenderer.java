b7be73ca5e8c88b0fd2b1403e5acbd51d38b358a
Aaron Haun <kj4ips@antichthon.net>
2014-05-16 19:35:43 -0400

    Auto Commit! Fri May 16 19:35:43 EDT 2014

--- src/main/java/guru/haun/hackery/client/render/BSODRenderer.java
+++ src/main/java/guru/haun/hackery/client/render/BSODRenderer.java
@@ -8,6 +8,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -29,12 +30,26 @@ public class BSODRenderer extends Gui {
 			int h = 256;
 			int xi = w >> 1;
 			int yi = h >> 1;
-			ResourceLocation res = new ResourceLocation("hackery:bsods/Sad Notch.png");
-			this.drawRect(0, 0, x, y, 0xFF000000);
+			
 			Tessellator tess = Tessellator.instance;
-			GL11.glColor4f(1F, 1F, 1F, 1F);
-			Minecraft.getMinecraft().renderEngine.bindTexture(res);
-			this.drawTexturedModalRect(xc-xi, yc-yi, 0, 0, w, h);
+			GL11.glEnable(GL11.GL_BLEND);
+			GL11.glDisable(GL11.GL_TEXTURE_2D);
+			OpenGlHelper.glBlendFunc(770, 771, 1, 0);
+			GL11.glColor4f(0, 0, 0, 1F);
+			
+			tess.startDrawingQuads();
+			tess.addVertex((double) 0, (double) y, 0D);
+			tess.addVertex((double) x, (double) y, 0D);
+			tess.addVertex((double) x, (double) 0, 0D);
+			tess.addVertex((double) 0, (double) 0, 0D);
+			tess.draw();
+			
+			GL11.glEnable(GL11.GL_TEXTURE_2D);
+			GL11.glDisable(GL11.GL_BLEND);
+			
+			//ResourceLocation res = new ResourceLocation("hackery:bsods/Sad Notch.png");
+			//Minecraft.getMinecraft().renderEngine.bindTexture(res);
+			//this.drawTexturedModalRect(xc-xi, yc-yi, 0, 0, w, h);
 		}
 	}
 	
