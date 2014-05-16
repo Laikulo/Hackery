004d428bd6d9b4f0ff956cbeb1efd62c85d05135
Aaron Haun <kj4ips@antichthon.net>
2014-05-16 17:35:28 -0400

    Auto Commit! Fri May 16 17:35:28 EDT 2014

--- src/main/java/guru/haun/hackery/client/render/BSODRenderer.java
+++ src/main/java/guru/haun/hackery/client/render/BSODRenderer.java
@@ -1,9 +1,14 @@
 package guru.haun.hackery.client.render;
 
+import org.lwjgl.opengl.GL11;
+
 import guru.haun.hackery.HackeryMod;
 import guru.haun.hackery.client.HacKeyBinds;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import cpw.mods.fml.common.gameevent.InputEvent;
 
@@ -16,6 +21,11 @@ public class BSODRenderer {
 			active = !active;
 		}
 		if(active){
+			int x = e.resolution.getScaledWidth();
+			int y = e.resolution.getScaledHeight();
+			GL11.glColor4f(1F, 1F, 1F, 1F);
+			Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("hackery:bsods/Sac_Mac.png"));
+			Tesselator tess = new Tessellator().instance;
 			
 		}
 	}
