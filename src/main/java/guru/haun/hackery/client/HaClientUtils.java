79f39730c963c91d99e93c65b9f359f59eef0e44
Aaron Haun <kj4ips@antichthon.net>
2014-05-17 16:45:18 -0400

    Auto Commit! Sat May 17 16:45:18 EDT 2014

---
+++ src/main/java/guru/haun/hackery/client/HaClientUtils.java
@@ -0,0 +1,25 @@
+package guru.haun.hackery.client;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import guru.haun.hackery.HackeryMod;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.shader.ShaderGroup;
+import net.minecraft.client.util.JsonException;
+import net.minecraft.util.ResourceLocation;
+
+public class HaClientUtils {
+
+	@SideOnly(Side.CLIENT)
+	public static void setShader(ResourceLocation sha){
+		Minecraft mc = Minecraft.getMinecraft();
+		try{
+			mc.entityRenderer.theShaderGroup = new ShaderGroup(mc.getResourceManager(), mc.getFramebuffer(), sha);
+		}catch(JsonException e){
+			HackeryMod.logger.error(String.format("Invalid Shader: %s:%s",sha.getResourceDomain(),sha.getResourcePath()));
+			e.printStackTrace();
+		}
+		mc.entityRenderer.theShaderGroup.createBindFramebuffers(mc.displayWidth, mc.displayHeight);
+	}
+	
+}
--- src/main/java/guru/haun/hackery/client/HacKeyHandler.java
+++ src/main/java/guru/haun/hackery/client/HacKeyHandler.java
@@ -14,15 +14,8 @@ public class HacKeyHandler {
 	@SubscribeEvent
 	public void onKeyEvent(InputEvent.KeyInputEvent e){
 		if(HacKeyBinds.debugkey.isPressed()){
-			ResourceLocation shader = new ResourceLocation("minecraft:shaders/post/wobble.json");
-			Minecraft mc = Minecraft.getMinecraft();
-			try {
-				mc.entityRenderer.theShaderGroup = new ShaderGroup(mc.getResourceManager(),mc.getFramebuffer(), shader);
-			} catch (JsonException e1) {
-				HackeryMod.logger.warn("Invalid Shader!");
-				e1.printStackTrace();
-			}
-			mc.entityRenderer.theShaderGroup.createBindFramebuffers(mc.displayWidth, mc.displayHeight);
+			ResourceLocation shader = new ResourceLocation("minecraft:shaders/post/bumpy.json");
+			HaClientUtils.setShader(shader);
 		}
 	}
 	
