602bacadcf0a36776cde8a69aa835fc3cefba7fb
Aaron Haun <kj4ips@antichthon.net>
2014-05-17 15:15:09 -0400

    Auto Commit! Sat May 17 15:15:09 EDT 2014

--- src/main/java/guru/haun/hackery/client/HacKeyHandler.java
+++ src/main/java/guru/haun/hackery/client/HacKeyHandler.java
@@ -1,6 +1,9 @@
 package guru.haun.hackery.client;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.shader.ShaderGroup;
+import net.minecraft.client.util.JsonException;
+import net.minecraft.util.ResourceLocation;
 import guru.haun.hackery.HackeryMod;
 import guru.haun.hackery.blocks.gui.GuiBSOD;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -11,7 +14,15 @@ public class HacKeyHandler {
 	@SubscribeEvent
 	public void onKeyEvent(InputEvent.KeyInputEvent e){
 		if(HacKeyBinds.debugkey.isPressed()){
-			Minecraft.getMinecraft().displayGuiScreen(new GuiBSOD());
+			ResourceLocation shader = new ResourceLocation("minecraft:shaders/post/wobble.json");
+			Minecraft mc = Minecraft.getMinecraft();
+			try {
+				mc.entityRenderer.theShaderGroup = new ShaderGroup(mc.getResourceManager(),mc.getFramebuffer(), shader);
+			} catch (JsonException e1) {
+				HackeryMod.logger.warn("Invalid Shader!");
+				e1.printStackTrace();
+			}
+			mc.entityRenderer.theShaderGroup.createBindFramebuffers(mc.displayWidth, mc.displayHeight);
 		}
 	}
 	
--- src/main/java/guru/haun/hackery/potion/HackPotionHandler.java
+++ src/main/java/guru/haun/hackery/potion/HackPotionHandler.java
@@ -14,30 +14,34 @@ public class HackPotionHandler {
 	private static boolean laststate = false;
 	private static long lasttick = 0;
 	private static int nexttick = 10;
+	private static float oldeye;
 	
 	@SubscribeEvent
 	public void onClientTick(TickEvent.ClientTickEvent e){
 		Minecraft mc = Minecraft.getMinecraft();
 		if(mc.thePlayer == null) return;
-		if(mc.thePlayer.isPotionActive(HackPotions.potionGF)){
-			long ticks = mc.theWorld.getWorldTime();
-			if(ticks != lasttick && ( ticks % (20 + nexttick) == 0)){
-				mc.entityRenderer.activateNextShader();
-				lasttick = ticks;
-				nexttick = (int) (20f * Math.random());
-			}
-			mc.thePlayer.eyeHeight = (float) (1.9f * Math.random() + .1f);
-		}
 		if(laststate != mc.thePlayer.isPotionActive(HackPotions.potionGF)){
 			if(mc.thePlayer.isPotionActive(HackPotions.potionGF)){
 				//On activateion
 				mc.entityRenderer.activateNextShader();
+				oldeye = mc.thePlayer.eyeHeight;
 			}else{
 				//On deactivation
 				mc.entityRenderer.deactivateShader();
+				mc.thePlayer.eyeHeight = oldeye;
 			}
 			laststate = mc.thePlayer.isPotionActive(HackPotions.potionGF);
 		}
+		if(mc.thePlayer.isPotionActive(HackPotions.potionGF)){
+			long ticks = mc.theWorld.getWorldTime();
+			if(ticks != lasttick && ( ticks % (20 + nexttick) == 0)){
+				mc.entityRenderer.activateNextShader();
+				mc.thePlayer.eyeHeight = (float) (1.9f * Math.random() + .1f);
+				lasttick = ticks;
+				nexttick = (int) (20f * Math.random());
+			}
+		}
+		
 		
 	}
 	
