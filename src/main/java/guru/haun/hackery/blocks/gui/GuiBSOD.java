389d9a58f0e9f03c36ff1a157a7a79b1fd611902
Aaron Haun <kj4ips@antichthon.net>
2014-05-16 21:05:53 -0400

    Auto Commit! Fri May 16 21:05:54 EDT 2014

--- src/main/java/guru/haun/hackery/blocks/gui/GuiBSOD.java
+++ src/main/java/guru/haun/hackery/blocks/gui/GuiBSOD.java
@@ -1,7 +1,13 @@
 package guru.haun.hackery.blocks.gui;
 
+import guru.haun.hackery.HackeryMod;
 import net.minecraft.client.gui.GuiScreen;
 
 public class GuiBSOD extends GuiScreen {
-
+	
+	@Override
+	public void keyTyped(char c, int i){
+		HackeryMod.logger.info(String.format("c %d i %d", (int)c, i));
+	}
+	
 }
--- src/main/java/guru/haun/hackery/client/HacKeyHandler.java
+++ src/main/java/guru/haun/hackery/client/HacKeyHandler.java
@@ -1,6 +1,8 @@
 package guru.haun.hackery.client;
 
+import net.minecraft.client.Minecraft;
 import guru.haun.hackery.HackeryMod;
+import guru.haun.hackery.blocks.gui.GuiBSOD;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.InputEvent;
 
@@ -9,7 +11,7 @@ public class HacKeyHandler {
 	@SubscribeEvent
 	public void onKeyEvent(InputEvent.KeyInputEvent e){
 		if(HacKeyBinds.debugkey.isPressed()){
-			;
+			Minecraft.getMinecraft().displayGuiScreen(new GuiBSOD());
 		}
 	}
 	
