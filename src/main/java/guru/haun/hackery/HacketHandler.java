d2b1dfe2f410a2cc10f1a36ad39af4a69f87cd2a
Aaron Haun <kj4ips@antichthon.net>
2014-05-16 23:06:10 -0400

    Auto Commit! Fri May 16 23:06:10 EDT 2014

--- src/main/java/guru/haun/hackery/HacketHandler.java
+++ src/main/java/guru/haun/hackery/HacketHandler.java
@@ -9,6 +9,26 @@ import cpw.mods.fml.common.network.FMLIndexedMessageToMessageCodec;
 
 public class HacketHandler extends FMLIndexedMessageToMessageCodec<IPacket>{
 
+	public static void writeString(ByteBuf buff, String string){
+		if(string != null) {
+			buff.writeInt(0);
+		}else{
+			for(int i = 0 ;i < string.length() ; i++){
+				buff.writeChar(string.toCharArray()[i]);
+			}
+		}
+	}
+	
+	public static String readString(ByteBuf buff){
+		String string;
+		int size = buff.readInt();
+		string = "";
+		for(int i = 0; i < size; i++){
+			string += buff.readChar();
+		}
+		return string;
+	}
+	
 	public HacketHandler()
 	{
 		addDiscriminator(0, setBSODPacket.class);
--- src/main/java/guru/haun/hackery/network/playerBSOD.java
+++ src/main/java/guru/haun/hackery/network/playerBSOD.java
@@ -1,8 +1,12 @@
 package guru.haun.hackery.network;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
 import cpw.mods.fml.common.network.FMLOutboundHandler;
 import cpw.mods.fml.relauncher.Side;
 import guru.haun.hackery.HackeryMod;
+import guru.haun.hackery.HacketHandler;
 import io.netty.buffer.ByteBuf;
 
 public class playerBSOD implements IPacket {
@@ -11,17 +15,22 @@ public class playerBSOD implements IPacket {
 	private String recipient;
 	@Override
 	public void readBytes(ByteBuf bytes) {
-		// TODO Auto-generated method stub
-		
+		attacker = HacketHandler.readString(bytes);
+		recipient = HacketHandler.readString(bytes);
 	}
 	@Override
 	public void writeBytes(ByteBuf bytes) {
-		// TODO Auto-generated method stub
+		HacketHandler.writeString(bytes, attacker);
+		HacketHandler.writeString(bytes, recipient);
 		
 	}
 	@Override
 	public void postProcess() {
+		EntityPlayerMP atk = MinecraftServer.getServer().getConfigurationManager().getPlayerForUsername(attacker);
+		EntityPlayerMP vic = MinecraftServer.getServer().getConfigurationManager().getPlayerForUsername(recipient);
 		HackeryMod.channel.get(Side.SERVER).attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.PLAYER);
+		HackeryMod.channel.get(Side.SERVER).attr(FMLOutboundHandler.FML_MESSAGETARGETARGS).set(atk);
+		HackeryMod.channel.get(Side.SERVER).writeOutbound(new setBSODPacket(true));
 	
 	}
 
