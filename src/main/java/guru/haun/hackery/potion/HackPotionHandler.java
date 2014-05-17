fd45cae8a091b9b313f3b9f59a896ccd9a5251b8
Aaron Haun <kj4ips@antichthon.net>
2014-05-17 17:30:27 -0400

    Auto Commit! Sat May 17 17:30:27 EDT 2014

--- src/main/java/guru/haun/hackery/potion/HackPotionHandler.java
+++ src/main/java/guru/haun/hackery/potion/HackPotionHandler.java
@@ -8,6 +8,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 
@@ -17,15 +18,24 @@ public class HackPotionHandler {
 	private static long lasttick = 0;
 	private static int nexttick = 10;
 	private static int nextheadtick = 5;
-	private static int lastheadtick = 0;
+	private static long lastheadtick = 0;
 	private static float oldeye;
 	
-	private static final String[] niceShaders = new String[] {"bits","bumpy","deconvolve","green","ntsc","phosphor","sobel"};
+	private static final String[] niceShaders = new String[] {"bits","bumpy","deconverge","green","ntsc","phosphor","sobel"};
 	
 	private void shuffleShader(){
 		String shadername = niceShaders[(int) ((float)niceShaders.length * Math.random())];
 		shadername = String.format("minecraft:shaders/post/%s.json",shadername);
 		HaClientUtils.setShader(new ResourceLocation(shadername));
+	}
+	
+	private void jerkHead(){
+		Minecraft mc = Minecraft.getMinecraft();
+		EntityPlayer player = mc.thePlayer;
+		HackeryMod.logger.info(String.format("Before: %d %d", player.rotationPitch, player.rotationYawHead));
+		player.rotationYawHead =  (player.rotationYawHead + (20f * (float) Math.random()));
+		player.rotationPitch = (player.rotationPitch + (20f * (float) Math.random()));
+		HackeryMod.logger.info(String.format("After: %d %d", player.rotationPitch, player.rotationYawHead));
 	}
 	
 	@SubscribeEvent
@@ -50,6 +60,7 @@ public class HackPotionHandler {
 				nexttick = (int) (20f * Math.random());
 			}
 			if(ticks != lastheadtick && (ticks % 10 + nextheadtick) == 0){
+				HackeryMod.logger.info("Ping");
 				jerkHead();
 				lastheadtick = ticks;
 				nextheadtick = (int) (10f * Math.random());
