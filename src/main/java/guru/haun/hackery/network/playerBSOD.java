18299d0123a051f123a4e094857651f2c6515ffc
Aaron Haun <kj4ips@antichthon.net>
2014-05-16 22:51:08 -0400

    Auto Commit! Fri May 16 22:51:08 EDT 2014

--- src/main/java/guru/haun/hackery/network/playerBSOD.java
+++ src/main/java/guru/haun/hackery/network/playerBSOD.java
@@ -1,8 +1,28 @@
 package guru.haun.hackery.network;
 
+import cpw.mods.fml.common.network.FMLOutboundHandler;
+import cpw.mods.fml.relauncher.Side;
+import guru.haun.hackery.HackeryMod;
+import io.netty.buffer.ByteBuf;
+
 public class playerBSOD implements IPacket {
 	
 	private String attacker;
 	private String recipient;
+	@Override
+	public void readBytes(ByteBuf bytes) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void writeBytes(ByteBuf bytes) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void postProcess() {
+		HackeryMod.channel.get(Side.SERVER).attr(FMLOutboundHandler.FML_MESSAGETARGET).set(FMLOutboundHandler.OutboundTarget.PLAYER);
+	
+	}
 
 }
