865fbf89ef81086a3d30fe42c9511dff7c57ec10
Aaron Haun <kj4ips@antichthon.net>
2014-05-17 14:45:04 -0400

    Auto Commit! Sat May 17 14:45:05 EDT 2014

--- src/main/java/guru/haun/hackery/potion/HackPotionHandler.java
+++ src/main/java/guru/haun/hackery/potion/HackPotionHandler.java
@@ -10,20 +10,23 @@ import net.minecraftforge.client.event.RenderGameOverlayEvent;
 public class HackPotionHandler {
 
 	private static boolean laststate = false;
+	private long lasttick = 0;
 	
 	@SubscribeEvent
 	@EventHandler
 	public void onRenderGameOverlay(RenderGameOverlayEvent e){
 		Minecraft mc = Minecraft.getMinecraft();
 		if(mc.thePlayer.isPotionActive(HackPotions.potionGF)){
-			int ticks = (int) e.partialTicks;
-			if(ticks % 80 == 0){
+			long ticks = mc.theWorld.getWorldTime();
+			if(ticks != lasttick && ticks % 80 == 0){
 				mc.entityRenderer.activateNextShader();
+				lasttick = ticks;
 			}
 		}
 		if(laststate != mc.thePlayer.isPotionActive(HackPotions.potionGF)){
 			if(mc.thePlayer.isPotionActive(HackPotions.potionGF)){
 				//On activateion
+				mc.entityRenderer.activateNextShader();
 			}else{
 				//On deactivation
 				mc.entityRenderer.deactivateShader();
