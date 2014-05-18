6f21fa13ead1d2355bcf4e3c03f862a8e4756696
Aaron Haun <kj4ips@antichthon.net>
2014-05-18 14:39:58 -0400

    Auto Commit! Sun May 18 14:39:58 EDT 2014

--- src/main/java/guru/haun/hackery/enchant/InfectedEnchantHandler.java
+++ src/main/java/guru/haun/hackery/enchant/InfectedEnchantHandler.java
@@ -1,9 +1,13 @@
 package guru.haun.hackery.enchant;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import guru.haun.hackery.ExploitUtils;
+import guru.haun.hackery.exploits.IExploit;
+import guru.haun.hackery.exploits.InfectionExploit;
 import guru.haun.hackery.items.ExploitItem;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemExpBottle;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 
 /**
@@ -13,6 +17,11 @@ public class InfectedEnchantHandler {
     @SubscribeEvent
     public void onDamage(LivingAttackEvent e){
         if (!(e.entity instanceof EntityPlayerMP)) return;
-        if (!(e.entity.))
+        ItemStack is = ((EntityPlayerMP) e.entity).getHeldItem();
+        if (is == null) return;
+        if (is.getItem() == null) return;
+        if (!(is.getItem() instanceof ExploitItem)) return;
+        if (ExploitUtils.getExploitById(is.getItemDamage()).getName() != "exploitVirus") return;
+        e.
     }
 }
