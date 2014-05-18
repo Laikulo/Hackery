c2456491a17c9ea6cff9f0e2191171af61c0dc2d
Aaron Haun <kj4ips@antichthon.net>
2014-05-17 20:52:39 -0400

    Auto Commit! Sat May 17 20:52:39 EDT 2014

--- src/main/java/guru/haun/hackery/items/GlitchPowder.java
+++ src/main/java/guru/haun/hackery/items/GlitchPowder.java
@@ -1,6 +1,7 @@
 package guru.haun.hackery.items;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.HashMap;
 
 import guru.haun.hackery.HackeryMod;
@@ -18,6 +19,7 @@ public class GlitchPowder extends Item{
 		if(register){
 			GameRegistry.registerItem(this, "glitchPowder");
 			GameRegistry.addShapelessRecipe(new ItemStack(this, 1,0), new ItemStack(HackeryMod.shardGlitch,1,0), new ItemStack(HackeryMod.shardGlitch,1,0), new ItemStack(HackeryMod.shardGlitch,1,0), new ItemStack(HackeryMod.shardGlitch,1,0));
+			registerPotion();
 		}
 	}
 	
@@ -27,7 +29,7 @@ public class GlitchPowder extends Item{
 	}
 	
 	public String getPotionEffect(ItemStack is){
-		return "+0+1-2+3";
+		return "+0+1-2+3-13";
 	}
 	
 	public void registerPotion(){
@@ -36,9 +38,13 @@ public class GlitchPowder extends Item{
 			Field potFields[] = potHepler.getDeclaredFields();
 			for(Field f : potFields){
 				if(f.getName() == "potionRequirements" || f.getName() == "field_77927_l"){
+					Field mods = Field.class.getDeclaredField("modifiers");
+					mods.setAccessible(true);
+					mods.setInt(f, f.getModifiers() & ~Modifier.FINAL);
 					f.setAccessible(true);
-					HashMap requireMap = (HashMap) f.get(new PotionHelper());
-					requireMap.put(Integer.valueOf(HackPotions.potionGF.getId()), "0 1 !2 3 !13")
+					final HashMap<Integer,String> newReq = (HashMap<Integer,String>)((HashMap<Integer,String>)f.get(null)).clone();
+					newReq.put(Integer.valueOf(HackPotions.potionGFId), "0 1 !2 3 !13");
+					f.set(null, newReq);
 				}else if(f.getName() == "potionAmplifiers" || f.getName() == "field_77928_m"){
 					;
 				}
