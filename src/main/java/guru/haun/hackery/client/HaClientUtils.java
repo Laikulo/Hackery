25828a27ee8d91d61023ca78c61ef73e580b68a2
Aaron Haun <aaron@haun.guru>
2014-05-29 22:14:12 -0400

    More JSONhell

--- src/main/java/guru/haun/hackery/client/HaClientUtils.java
+++ src/main/java/guru/haun/hackery/client/HaClientUtils.java
@@ -13,12 +13,12 @@ public class HaClientUtils {
 	@SideOnly(Side.CLIENT)
 	public static void setShader(ResourceLocation sha){
 		Minecraft mc = Minecraft.getMinecraft();
-        //try {
+        try {
             mc.entityRenderer.theShaderGroup = new ShaderGroup(mc.getResourceManager(), mc.getFramebuffer(), sha);
-        //} catch (JsonException e) {
-         //   HackeryMod.logger.error("Something bad happened while swapping shaders");
-         //   e.printStackTrace();
-        //}
+        } catch (JsonException e) {
+            HackeryMod.logger.error("Something bad happened while swapping shaders");
+            e.printStackTrace();
+        }
         mc.entityRenderer.theShaderGroup.createBindFramebuffers(mc.displayWidth, mc.displayHeight);
 	}
 	
